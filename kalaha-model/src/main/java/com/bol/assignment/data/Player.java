365cd3c917318c1bd4207ba63cc4aa53c2931e42
ravi <ravihj5@gmail.com>
2023-11-22 04:18:02 +0100

    Remove final from Player record

--- kalaha-model/src/main/java/com/bol/assignment/data/Player.java
+++ kalaha-model/src/main/java/com/bol/assignment/data/Player.java
@@ -1,9 +1,9 @@
 package com.bol.assignment.data;
 
 
-public final record Player (int index, String name, House house){
-    public static final int NONE = 0;
-    public static final int PLAYER1 = 1;
-    public static final int PLAYER2 = 2;
-    public  static final int GAME_OVER = 3;
+public record Player (int index, String name, House house){
+    public static int NONE = 0;
+    public static int PLAYER1 = 1;
+    public static int PLAYER2 = 2;
+    public  static int GAME_OVER = 3;
 }
